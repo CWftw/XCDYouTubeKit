c4232cba422cae8d7f84a5e28b5b728edeb0d411
Colin Willson <Colin@CWftw.me>
2020-09-10 12:17:17 -0400

    Add restricted status of the webpage to the XCDYouTubeVideo object

--- XCDYouTubeKit/XCDYouTubeVideoOperation.m
+++ XCDYouTubeKit/XCDYouTubeVideoOperation.m
@@ -272,6 +272,7 @@ static NSError *YouTubeError(NSError *error, NSSet *regionsAllowed, NSString *la
 			return;
 		}
 		[video mergeVideo:self.noStreamVideo];
+		video.isAgeRestricted = self.webpage.isAgeRestricted;
 		[self finishWithVideo:video];
 	}
 	else
